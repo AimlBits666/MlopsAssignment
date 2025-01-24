e27cd3380c4fc233a8cc8b5f741232fa88e1bd86
mkmk1616 <mkmkmacro@gmail.com>
2025-01-24 16:42:02 +0530

    stop tracking data\mobile_data.csv

---
+++ src/train.py
@@ -0,0 +1,50 @@
+import mlflow.artifacts
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error,mean_squared_error
+from sklearn.model_selection import train_test_split
+import joblib
+
+import mlflow
+import mlflow.sklearn
+
+data = pd.read_csv(r'D:\Mukesh\pycharm_project\Mlops_Pro\assignment\MlopsAssignment\data\mobile_data.csv')
+data = data.drop(['three_g'],axis=1)
+
+x = data.iloc[:,:-1]
+y = data['price_range']
+
+X_train,X_test,Y_train,Y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+
+
+
+mlflow.set_experiment("first model")
+with mlflow.start_run():
+    random_state = 1
+    max_depth=5
+    min_samples_split=10
+
+    mlflow.log_param("random_state",random_state)
+    mlflow.log_param("max_depth",max_depth)
+    mlflow.log_param("min_samples_split",min_samples_split)
+
+
+    TreeModel = DecisionTreeRegressor(random_state=random_state,max_depth=max_depth,min_samples_split=min_samples_split)
+    # Fit Model
+    TreeModel.fit(X_train, Y_train)
+
+
+    val_predictions = TreeModel.predict(X_test)
+    val_mae = mean_absolute_error(val_predictions, Y_test)
+    val_mse = mean_squared_error(val_predictions, Y_test)
+
+    mlflow.log_metric("Mean absolute Error",val_mae)
+    mlflow.log_metric("Mean Square error",val_mae)
+
+    mlflow.sklearn.log_model(TreeModel,"MY_model")
+    joblib.dump(TreeModel,r"D:\Mukesh\pycharm_project\Mlops_Pro\assignment\MlopsAssignment\model\my_model.joblib")
+    mlflow.log_artifact(r"D:\Mukesh\pycharm_project\Mlops_Pro\assignment\MlopsAssignment\model\MY_model.joblib")
+
+    print("Validation MAE: {:,.0f}".format(val_mae))
+    print("Validation MAE: {:,.0f}".format(val_mse))
